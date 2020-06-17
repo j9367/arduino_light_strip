3944f72b233356bc9b9a8a30ea548995f1001c6e
vas-byte <54138790+vas-byte@users.noreply.github.com>
2020-06-17 11:44:52 +0930

    Create Xcode Project

---
+++ Xcode Project/Light control/ViewController.swift
@@ -0,0 +1,249 @@
+//
+//  ViewController.swift
+//  Light control
+//
+//  Created by Vasilis Michalakis on 17/6/20.
+//  Copyright © 2020 Vasilis Michalakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var Pattern1Button: UIView!
+    
+    @IBOutlet weak var OFF: UIButton!
+    
+    @IBOutlet weak var Strobe: UIButton!
+    
+    @IBOutlet weak var Police: UIButton!
+    
+    @IBOutlet weak var RED: UIButton!
+    
+    @IBOutlet weak var Green: UIButton!
+    
+    @IBOutlet weak var Blue: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       Pattern1Button.layer.cornerRadius = 4
+        OFF.layer.cornerRadius = 4
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+ 
+    @IBAction func Pattern1Button(_ sender: UIButton) {
+        let url = URL(string: "http://172.20.10.11/Rainbow")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    @IBAction func OFF(_ sender: Any) {
+        let url = URL(string: "http://172.20.10.11/LED=OFF")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    
+    @IBAction func Strobe(_ sender: Any) {
+        let url = URL(string: "http://172.20.10.11/pattern3")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    @IBAction func Police(_ sender: Any) {
+        let url = URL(string: "http://172.20.10.11/pattern2")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    @IBAction func RED(_ sender: Any) {
+        let url = URL(string: "http://172.20.10.11/Strip=RED")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    @IBAction func Green(_ sender: Any) {
+        let url = URL(string: "http://172.20.10.11/Strip=Green")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    @IBAction func Blue(_ sender: Any) {
+        let url = URL(string: "http://172.20.10.11/Strip=Blue")
+        guard let requestUrl = url else { fatalError() }
+        // Prepare URL Request Object
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        
+        // HTTP Request Parameters which will be sent in HTTP Request Body
+        let postString = "";
+        // Set HTTP Request Body
+        request.httpBody = postString.data(using: String.Encoding.utf8);
+        // Perform HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check for Error
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Convert HTTP Response Data to a String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+        }
+        print("button works")
+        task.resume()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+  
+}
+
+
+
