cb84beba0cfacd5eeaf68590aa7c78c9f9706ac3
vas-byte <54138790+vas-byte@users.noreply.github.com>
2020-06-19 12:06:35 +0930

    Colour Picker now works
    Colour Picker works we have a ton of bugs

---
+++ Light control/Light control/SecondView.swift
@@ -0,0 +1,38 @@
+//
+//  SecondView.swift
+//  Light control
+//
+//  Created by Vasilis Michalakis on 19/6/20.
+//  Copyright © 2020 Vasilis Michalakis. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class SecondView: UIViewController, WKUIDelegate {
+
+     @IBOutlet var colourPicker: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+       colourPicker = WKWebView(frame: .zero, configuration: webConfiguration)
+        colourPicker.uiDelegate = self
+        view = colourPicker
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"http://172.20.10.11/colopicke")
+        let myRequest = URLRequest(url: myURL!)
+       colourPicker.load(myRequest)
+    }}
+    /*
+     @IBOutlet weak var colourPicker: WKWebView!
+     // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+
