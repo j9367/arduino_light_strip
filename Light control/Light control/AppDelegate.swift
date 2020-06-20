ce1209f4ade0ed2fb7c1463ae7bd7f1fb8b573fa
vas-byte <54138790+vas-byte@users.noreply.github.com>
2020-06-20 18:09:48 +0930

    New Stuff
    Fixed more bugs related to some threads + Sign Status now works it doesn't update during light patterns due to the arduino however but i can work on that the Activity indicator does not work yet however

--- Light control/Light control/AppDelegate.swift
+++ Light control/Light control/AppDelegate.swift
@@ -9,12 +9,19 @@
 import UIKit
 import CoreData
 
+
+
+
+
+  
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+   
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -26,8 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -89,5 +95,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
-}
 
+}
--- Light control/Light control/ViewController.swift
+++ Light control/Light control/ViewController.swift
@@ -8,17 +8,25 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
-    
-    
-    
-    
+
     @IBOutlet weak var Status: UILabel!
+    
+    @IBOutlet weak var AI: UIImageView!
+    
+    @IBOutlet weak var Idle: UILabel!
     
     
     @IBOutlet weak var Pattern1Button: UIButton!
     
-    @IBOutlet weak var progressBarView: UIProgressView!
+    @IBOutlet weak var signStatus: UIImageView!
+    
+    
+    @IBOutlet weak var activityIndicator: UIImageView!
+    
+    @IBOutlet weak var CP: UIButton!
     
     @IBOutlet weak var OFF: UIButton!
     
@@ -32,14 +40,23 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var Blue: UIButton!
 
+ 
+   
+    
     
     override func viewDidLoad() {
-     progressBarView.progress = 0.0
+    self.signStatus.image?.withRenderingMode(.alwaysTemplate)
+         self.signStatus.tintColor = UIColor.gray
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //test
+            checkIsConnectedToNetwork()
     }
+    
 
+    
+  
+    
+   
     @IBAction func Pattern1Button(_ sender: UIButton) {
         LEDOFF()
          self.Status.text = "Rainbow"
@@ -48,7 +65,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-        self.progressBarView.progress = 0.65
+  
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -56,27 +73,14 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            self.progressBarView.progress = 0.8
+          
             
            
             
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-              
-            }
-            
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
         }
-        self.progressBarView.progress = 1.0
-          self.progressBarView.progress = 0.0
+       
         print("button works")
+ 
         task.resume()
     }
     
@@ -87,7 +91,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-        self.progressBarView.progress = 0.65
+   
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -95,28 +99,17 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-        self.progressBarView.progress = 0.8
+       
             
             
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-            }
             
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
         }
         print("button works")
-         self.progressBarView.progress = 1.0
-        self.progressBarView.progress = 0.0
+          checkIsConnectedToNetwork()
        task.resume()
     }
     
+     
     
     @IBAction func Strobe(_ sender: Any) {
         LEDOFF()
@@ -126,7 +119,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-            self.progressBarView.progress = 0.6
+            
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -134,26 +127,16 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-             self.progressBarView.progress = 0.8
             
             
             
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                 self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-            }
             
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
+            
+            
         }
-         self.progressBarView.progress = 1.0
-          self.progressBarView.progress = 0.0
+         
         print("button works")
+    
         task.resume()
     }
     
@@ -165,7 +148,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-        self.progressBarView.progress = 0.6
+       
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -173,26 +156,14 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            self.progressBarView.progress = 0.8
+            
             
            
             
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-            }
             
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
         }
         print("button works")
-        self.progressBarView.progress = 1.0
-          self.progressBarView.progress = 0.0
+     
         task.resume()
     }
     
@@ -204,7 +175,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-           self.progressBarView.progress = 0.6
+          
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -212,26 +183,13 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-               self.progressBarView.progress = 0.8
+              
             
            
-            
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-            }
-            
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
+       
         }
         print("button works")
-        self.progressBarView.progress = 1.0
-          self.progressBarView.progress = 0.0
+      
         task.resume()
     }
     
@@ -243,7 +201,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-        self.progressBarView.progress = 0.6
+       
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -251,26 +209,13 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-           self.progressBarView.progress = 0.8
+          
             
             
-            
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-            }
-            
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
+         
         }
         print("button works")
-        self.progressBarView.progress = 1.0
-          self.progressBarView.progress = 0.0
+      
         task.resume()
     }
     
@@ -282,7 +227,7 @@ class ViewController: UIViewController {
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-        self.progressBarView.progress = 0.6
+      
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -290,26 +235,14 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-           self.progressBarView.progress = 0.8
+          
             
            
             
-            // Check for Error
-            if let error = error {
-                print("Error took place \(error)")
-                self.progressBarView.progress = 1.0
-                  self.progressBarView.progress = 0.0
-                return
-            }
-            
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
+        
         }
         print("button works")
-        self.progressBarView.progress = 1.0
-          self.progressBarView.progress = 0.0
+    
         task.resume()
     }
     
@@ -320,13 +253,13 @@ class ViewController: UIViewController {
     }
    
     func LEDOFF(){
-        progressBarView.progress = 0.0
+       
         let url = URL(string: "http://172.20.10.11/LED=OFF")
         guard let requestUrl = url else { fatalError() }
         // Prepare URL Request Object
         var request = URLRequest(url: requestUrl)
         request.httpMethod = "POST"
-        progressBarView.progress = 0.125
+        
         
         // HTTP Request Parameters which will be sent in HTTP Request Body
         let postString = "";
@@ -334,27 +267,83 @@ class ViewController: UIViewController {
         request.httpBody = postString.data(using: String.Encoding.utf8);
         // Perform HTTP Request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-         self.progressBarView.progress = 0.25
-            // Check for Error
-           
-            
-            // Convert HTTP Response Data to a String
-            if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                print("Response data string:\n \(dataString)")
-            }
         }
+       
         print("button works")
-         self.progressBarView.progress = 0.5
+         
         task.resume()
     }
   
     
+    @IBAction func CP(_ sender: Any) {
+    
+    
+    let url = URL(string: "http://172.20.10.11/colopicke")
+     guard let requestUrl = url else { fatalError() }
+     // Prepare URL Request Object
+     var request = URLRequest(url: requestUrl)
+     request.httpMethod = "POST"
+     
+     
+     // HTTP Request Parameters which will be sent in HTTP Request Body
+     let postString = "";
+     // Set HTTP Request Body
+     request.httpBody = postString.data(using: String.Encoding.utf8);
+     // Perform HTTP Request
+     let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+     }
+    
+     print("button works")
+     
+     task.resume()
 
+
+}
+   
+    
+    func checkIsConnectedToNetwork() {
+       let hostUrl: String = "http://172.20.10.11"
+       if let url = URL(string: hostUrl) {
+          var request = URLRequest(url: url)
+          request.httpMethod = "HEAD"
+          URLSession(configuration: .default)
+          .dataTask(with: request) { (_, response, error) -> Void in
+             guard error == nil else {
+                print("Error:", error ?? "")
+            DispatchQueue.main.async {
+                self.signStatus.image?.withRenderingMode(.alwaysTemplate)
+            self.signStatus.tintColor = UIColor.red
+                }
+                return
+             
+            }
+             guard (response as? HTTPURLResponse)?
+             .statusCode == 200 else {
+                print("The host is down")
+                 DispatchQueue.main.async {
+                self.signStatus.image?.withRenderingMode(.alwaysTemplate)
+                           self.signStatus.tintColor = UIColor.red
+                }
+                return
+             }
+             print("The host is up and running")
+            DispatchQueue.main.async {
+            self.signStatus.image?.withRenderingMode(.alwaysTemplate)
+            self.signStatus.tintColor = UIColor.green
+            }
+            
+        }
+        
+        .resume()
+        }
+        }
+ 
 }
 
 
 
 
 
-   
+  
+
 
